81f783f62de7ea90a94e3fc5954dfb8776d17d21
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-01-27 23:39:24 -0500

    added basic structure styling

--- src/App.js
+++ src/App.js
@@ -9,15 +9,26 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Kayla's Portfolio</h1>
+      <header>
+        <div className="logo">
+            <h1>Kayla Famurewa</h1>
+        </div>
+        <div className='menu'>
+            <nav>
+                <a href="#philosophy">01. Philosophy</a>
+                <a href="#">0.2 Experience</a>
+                <a href="#">0.3 Projects</a>
+                <a href="#">0.4 Contact</a>
+            </nav>
+        </div>
       </header>
-      <Home/>
-      <Philosophy/>
-      <Experience/>
-      <Projects/>
-      <Contact/>
-      
+      <div className='container'>
+        <Home/>
+        <Philosophy/>
+        <Experience/>
+        <Projects/>
+        <Contact/>
+      </div>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import homeImg from './../assets/undraw_programmer_re_owql.svg';
 
 export default function Home() {
   return (<section className='one'>
-      <div>
+    <div>
         <h1>Fullstack Developer.</h1>
         <p>I like to craft solid and scalable frontend products with excellent UX.</p>
-      </div>
+    </div>
 
-      <div>
-
-      </div>
+    <div className="pageOneRight">
+        <div className="rightImg">
+        <img src={homeImg} alt='programmer illustration'/>
+        </div>
+    </div>
+      
   </section>)
 }
